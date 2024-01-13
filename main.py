f60fbef8d398b8ec967e575fe511eaec5d380e47
Eneko Perez Bascones <91752156+Enekopb@users.noreply.github.com>
2024-01-13 11:17:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-def suma(A, B):
-    return A + B
+def suma(a, b):
+    """
+    Suma dos números.
+
+    Args:
+        a (int): Primer número.
+        b (int): Segundo número.
+
+    Returns:
+        int: La suma de los dos números.
+    """
+    return a + b
 
 A = 2
 B = 3
